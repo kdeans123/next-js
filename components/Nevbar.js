9711dad1b812fac13c1afcfe06ad54a1597f8c48
Kat <kat.m.deans@gmail.com>
2025-02-13 12:02:07 -0500

    Start of the project

---
+++ components/Nevbar.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Navbar.modules.css"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>Home</ul>
+            <ul>Posts</ul>
+            <ul>Users</ul>
+        </nav>
+    )
+}
