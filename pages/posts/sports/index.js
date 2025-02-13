8286bea7e98d2597c55e24e3ea258f0611b12ebd
Kat <kat.m.deans@gmail.com>
2025-02-13 14:14:38 -0500

    Folders

---
+++ pages/posts/sports/index.js
@@ -0,0 +1,5 @@
+export default function Sports() {
+    return (
+        <h1>This is the sports page </h1>
+    )
+}
---
+++ pages/posts/sports/trending/jack/index.js
@@ -0,0 +1,5 @@
+export default function Jack() {
+    return (
+        <h1>These are the sports related trending posts by Jack </h1>
+    )
+}
